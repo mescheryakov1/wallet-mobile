b8e9687d5c3015c1e529a55f86b6862d2b63a92c
mescheryakov1 <mescheryakov@me.com>
2025-11-08 18:02:33 +0300

    Load Groovy namespace shim from separate script

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,6 +1,3 @@
-import com.android.build.gradle.BaseExtension
-import groovy.util.XmlSlurper
-
 allprojects {
     repositories {
         google()
@@ -8,40 +5,13 @@ allprojects {
     }
 }
 
+apply(from = "namespace-shim.gradle")
+
 val newBuildDir: Directory =
     rootProject.layout.buildDirectory
         .dir("../../build")
         .get()
 rootProject.layout.buildDirectory.value(newBuildDir)
-
-// --- BEGIN namespace shim for AGP 8.x ---
-subprojects { subproject ->
-    afterEvaluate {
-        if (subproject.plugins.hasPlugin("com.android.library") ||
-            subproject.plugins.hasPlugin("com.android.application")
-        ) {
-            val androidExt = subproject.extensions.findByName("android")
-            if (androidExt is BaseExtension) {
-                val hasNs = try {
-                    !androidExt.namespace.isNullOrBlank()
-                } catch (_: Exception) {
-                    false
-                }
-                if (!hasNs) {
-                    val manifestFile = androidExt.sourceSets.getByName("main").manifest.srcFile
-                    if (manifestFile != null && manifestFile.exists()) {
-                        val pkg = XmlSlurper().parse(manifestFile).getProperty("@package")?.toString()
-                        if (!pkg.isNullOrBlank()) {
-                            androidExt.namespace = pkg
-                            println("[namespace-shim] ${subproject.name} -> ${pkg}")
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
-// --- END namespace shim ---
 
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
