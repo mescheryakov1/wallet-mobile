d2fd38b7cd1d0df31ec612983b5bc918b1e2b7c0
mescheryakov1 <mescheryakov@me.com>
2025-11-08 22:25:37 +0300

    Replace uni_links with native Android deeplink channel

--- lib/core/wallet/walletconnect_deeplink.dart
+++ lib/core/wallet/walletconnect_deeplink.dart
@@ -1,28 +1,18 @@
-import 'dart:async';
-
-import 'package:uni_links/uni_links.dart';
-
 import '../../wallet_connect_service.dart';
+import '../deeplink/android_deeplink.dart' show DeeplinkAndroid;
 
 Future<void> handleInitialUriAndStream(WalletConnectService svc) async {
-  try {
-    final Uri? initial = await getInitialUri();
-    if (initial != null && initial.scheme == 'wc') {
-      await svc.connectFromUri(initial.toString());
-    }
-  } catch (_) {
-    // ignored: initial uri retrieval errors are non-fatal
-  }
+  await DeeplinkAndroid.init();
 
-  uriLinkStream.listen((Uri? uri) async {
-    if (uri != null && uri.scheme == 'wc') {
-      try {
-        await svc.connectFromUri(uri.toString());
-      } catch (_) {
-        // ignored: errors are handled by the service
-      }
+  DeeplinkAndroid.stream.listen((String link) async {
+    if (!link.startsWith('wc:')) {
+      return;
     }
-  }, onError: (_) {
-    // ignored: stream errors are non-fatal
+
+    try {
+      await svc.connectFromUri(link);
+    } catch (_) {
+      // ignored: errors are handled by the service
+    }
   });
 }
