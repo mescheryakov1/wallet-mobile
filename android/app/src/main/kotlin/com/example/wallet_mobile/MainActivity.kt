949ead59362156e9b73c9b4b399d7feae5aa7c5d
mescheryakov1 <mescheryakov@me.com>
2025-11-20 15:36:35 +0300

    Add Android deeplink method channel handler

--- android/app/src/main/kotlin/com/example/wallet_mobile/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/wallet_mobile/MainActivity.kt
@@ -8,7 +8,7 @@ import io.flutter.plugin.common.MethodChannel
 
 class MainActivity : FlutterActivity() {
     private val channelName = "deeplink"
-    private var channel: MethodChannel? = null
+    private var methodChannel: MethodChannel? = null
     private var initialLink: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,25 +18,31 @@ class MainActivity : FlutterActivity() {
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
-        channel = MethodChannel(flutterEngine.dartExecutor.binaryMessenger, channelName)
-        channel?.setMethodCallHandler { call, result ->
-            when (call.method) {
-                "getInitialLink" -> result.success(initialLink)
-                else -> result.notImplemented()
+        methodChannel = MethodChannel(flutterEngine.dartExecutor.binaryMessenger, channelName)
+            .apply {
+                setMethodCallHandler { call, result ->
+                    when (call.method) {
+                        "getInitialLink" -> {
+                            result.success(initialLink)
+                            initialLink = null
+                        }
+                        else -> result.notImplemented()
+                    }
+                }
             }
-        }
     }
 
     override fun onNewIntent(intent: Intent) {
         super.onNewIntent(intent)
+        setIntent(intent)
         handleIntent(intent)
     }
 
-    private fun handleIntent(i: Intent?) {
-        val data = i?.dataString ?: return
-        if (data.startsWith("wc:")) {
-            initialLink = data
-            channel?.invokeMethod("onLink", data)
+    private fun handleIntent(intent: Intent?) {
+        val link = intent?.dataString ?: return
+        if (initialLink == null) {
+            initialLink = link
         }
+        methodChannel?.invokeMethod("onLink", link)
     }
 }
